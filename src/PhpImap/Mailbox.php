3f5487b6730ec7e11c41d8be7b36c1ce5a3e45f0
Sebastian Fischer <typo3@evoweb.de>
2026-03-29 11:23:06 +0200

    Fix slash in attachment file name issue #676

--- src/PhpImap/Mailbox.php
+++ src/PhpImap/Mailbox.php
@@ -1358,6 +1358,7 @@ class Mailbox
             $fileName = $this->decodeMimeStr($fileName);
             $fileName = $this->decodeRFC2231($fileName);
         }
+        $fileName = str_replace('/', '_', $fileName);
 
         /** @var scalar|array|object|null */
         $sizeInBytes = $partStructure->bytes ?? null;
