f6d509c96d89644f0bbeb4515ce4cc7672690064
Sebastian Fischer <typo3@evoweb.de>
2026-03-31 21:23:28 +0200

    [TASK] Cleanup LiveMailboxIssue250Test.php

--- tests/Unit/LiveMailboxIssue250Test.php
+++ tests/Unit/LiveMailboxIssue250Test.php
@@ -11,63 +11,51 @@ declare(strict_types=1);
 
 namespace PhpImap\Tests\Unit;
 
-use Generator;
-use ParagonIE\HiddenString\HiddenString;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
 
 /**
- * @phpstan-type MAILBOX_ARGS = array{
- *	0:HiddenString,
- *	1:HiddenString,
- *	2:HiddenString,
- *	3:string,
- *	4?:string
- * }
- * @phpstan-type COMPOSE_ENVELOPE = array{
- *	subject?:string
- * }
- * @phpstan-type COMPOSE_BODY = list<array{
- *	type?:int,
- *	encoding?:int,
- *	charset?:string,
- *	subtype?:string,
- *	description?:string,
- *	disposition?:array{filename:string}
- * }>
+ * @phpstan-import-type MAILBOX_ARGS from AbstractLiveMailboxTest
+ * @phpstan-import-type COMPOSE_ENVELOPE from AbstractLiveMailboxTest
+ * @phpstan-import-type COMPOSE_BODY from AbstractLiveMailboxTest
  */
 class LiveMailboxIssue250Test extends AbstractLiveMailboxTest
 {
     use LiveMailboxAppendTestTrait;
 
     /**
-     * @phpstan-return Generator<int, array{0: array{subject: string}, 1: array{0: array{type: 0, 'contents.data': 'test'}}, 2: string}, mixed, void>
+     * @phpstan-return \Generator<int, array{
+     *      0: COMPOSE_ENVELOPE,
+     *      1: COMPOSE_BODY,
+     *      2: string
+     * }, mixed, void>
      */
     public static function ComposeProvider(): \Generator
     {
-        $random_subject = 'barbushin/php-imap#250 测试: ' . \bin2hex(\random_bytes(16));
+        $randomSubject = 'barbushin/php-imap#250 测试: ' . \bin2hex(\random_bytes(16));
 
         yield [
-            ['subject' => $random_subject],
+            ['subject' => $randomSubject],
             [
                 [
                     'type' => \TYPETEXT,
                     'contents.data' => 'test',
                 ],
             ],
-            (
-                'Subject: ' . $random_subject . "\r\n" .
-                'MIME-Version: 1.0' . "\r\n" .
-                'Content-Type: TEXT/PLAIN; CHARSET=US-ASCII' . "\r\n" .
-                "\r\n" .
-                'test' . "\r\n"
-            ),
+            implode(LF, [
+                'Subject: ' . $randomSubject,
+                'MIME-Version: 1.0',
+                'Content-Type: TEXT/PLAIN; CHARSET=US-ASCII',
+                '',
+                'test',
+                ''
+            ]),
         ];
     }
 
     /**
-     * @phpstan-param MAILBOX_ARGS $mailbox_args
+     * @phpstan-param MAILBOX_ARGS $mailboxArguments
      * @phpstan-param COMPOSE_ENVELOPE $envelope
      * @phpstan-param COMPOSE_BODY $body
      */
@@ -76,12 +64,12 @@ class LiveMailboxIssue250Test extends AbstractLiveMailboxTest
     #[Group('live')]
     #[Group('live-issue-250')]
     public function testAppend(
-        array $mailbox_args,
+        array $mailboxArguments,
         array $envelope,
         array $body,
-        string $_expected_compose_result,
-        bool $pre_compose
+        string $expectedComposeResult,
+        bool $preCompose
     ): void {
-        $this->runAppendTest($mailbox_args, $envelope, $body, $_expected_compose_result, $pre_compose);
+        $this->runAppendTest($mailboxArguments, $envelope, $body, $expectedComposeResult, $preCompose);
     }
 }
