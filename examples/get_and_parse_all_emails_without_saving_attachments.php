23a0daf06ad4d1e4ac0ab6c0ae8619b32ff26763
Sebastian Fischer <typo3@evoweb.de>
2026-04-06 10:39:36 +0200

    [TASK] Clean examples and exclude from qlty

--- examples/get_and_parse_all_emails_without_saving_attachments.php
+++ examples/get_and_parse_all_emails_without_saving_attachments.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use PhpImap\Exceptions\ConnectionException;
+use PhpImap\Exceptions\InvalidParameterException;
 use PhpImap\Mailbox;
 
 $mailbox = new Mailbox(
@@ -28,56 +29,59 @@ $mailbox = new Mailbox(
     '*********' // Password for the before configured username
 );
 
-// If you haven't defined the server encoding (charset) in 'new Mailbox()', you can change it any time
-$mailbox->setServerEncoding('US-ASCII');
-
 try {
-    $mail_ids = $mailbox->searchMailbox('UNSEEN');
-} catch (ConnectionException $ex) {
+    // If you haven't defined the server encoding (charset) in 'new Mailbox()', you can change it any time
+    $mailbox->setServerEncoding('US-ASCII');
+    $mailIds = $mailbox->searchMailbox('UNSEEN');
+} catch (ConnectionException | InvalidParameterException $ex) {
     exit('IMAP connection failed: ' . $ex->getMessage());
 }
 
-foreach ($mail_ids as $mail_id) {
-    echo "+------ P A R S I N G ------+\n";
+try {
+    foreach ($mailIds as $mailId) {
+        echo "+------ P A R S I N G ------+\n";
 
-    $email = $mailbox->getMail(
-        $mail_id, // ID of the email, you want to get
-        false // Do NOT mark emails as seen (optional)
-    );
+        $email = $mailbox->getMail(
+            $mailId, // ID of the email, you want to get
+            false // Do NOT mark emails as seen (optional)
+        );
 
-    echo 'from-name: ' . ($email->fromName ?? $email->fromAddress) . "\n";
-    echo 'from-email: ' . $email->fromAddress . "\n";
-    echo 'to: ' . $email->toString . "\n";
-    echo 'subject: ' . $email->subject . "\n";
-    echo 'message_id: ' . $email->messageId . "\n";
+        echo 'from-name: ' . ($email->fromName ?? $email->fromAddress) . "\n";
+        echo 'from-email: ' . $email->fromAddress . "\n";
+        echo 'to: ' . $email->toString . "\n";
+        echo 'subject: ' . $email->subject . "\n";
+        echo 'message_id: ' . $email->messageId . "\n";
 
-    echo 'mail has attachments? ';
-    if ($email->hasAttachments()) {
-        echo "Yes\n";
-    } else {
-        echo "No\n";
+        echo 'mail has attachments? ';
+        if ($email->hasAttachments()) {
+            echo "Yes\n";
+        } else {
+            echo "No\n";
+        }
+
+        if (!empty($email->getAttachments())) {
+            echo count($email->getAttachments()) . " attachements\n";
+        }
+        if ($email->textHtml) {
+            echo "Message HTML:\n" . $email->textHtml;
+        } else {
+            echo "Message Plain:\n" . $email->textPlain;
+        }
+
+        if (!empty($email->autoSubmitted)) {
+            // Mark email as "read" / "seen"
+            $mailbox->markMailAsRead($mailId);
+            echo "+------ IGNORING: Auto-Reply ------+\n";
+        }
+
+        if (!empty($email_content->precedence)) {
+            // Mark email as "read" / "seen"
+            $mailbox->markMailAsRead($mailId);
+            echo "+------ IGNORING: Non-Delivery Report/Receipt ------+\n";
+        }
     }
 
-    if (!empty($email->getAttachments())) {
-        echo count($email->getAttachments()) . " attachements\n";
-    }
-    if ($email->textHtml) {
-        echo "Message HTML:\n" . $email->textHtml;
-    } else {
-        echo "Message Plain:\n" . $email->textPlain;
-    }
-
-    if (!empty($email->autoSubmitted)) {
-        // Mark email as "read" / "seen"
-        $mailbox->markMailAsRead($mail_id);
-        echo "+------ IGNORING: Auto-Reply ------+\n";
-    }
-
-    if (!empty($email_content->precedence)) {
-        // Mark email as "read" / "seen"
-        $mailbox->markMailAsRead($mail_id);
-        echo "+------ IGNORING: Non-Delivery Report/Receipt ------+\n";
-    }
+    $mailbox->disconnect();
+} catch (ConnectionException | Exception $ex) {
+    echo 'Something went wrong: ' . $ex->getMessage();
 }
-
-$mailbox->disconnect();
